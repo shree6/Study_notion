a64a3a415629f3d44942e41623dce2916f591f94
Sahil Bhore <73897612+shree6@users.noreply.github.com>
2023-08-14 22:49:27 +0530

    Add files via upload

---
+++ server/models/Profile.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+
+    gender:{
+        type:String
+    },
+    dateOfBirth:{
+        type:Date,
+    },
+    about:{
+        type:String,
+        trim:true
+    },
+    contactNumber:{
+        type:String,
+        trim:truea
+    }
+
+});
+
+module.exports = mongoose.model("Profile",profileSchema);
